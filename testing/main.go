d0c6109e0f4b8cfb8e04eb10cc1e21d7ac6791bb
knqyf263 <knqyf263@gmail.com>
2018-02-03 18:16:03 +0900

    Add testing

---
+++ testing/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/xml"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+)
+
+// CpeList is CPE list
+type CpeList struct {
+	CpeItems []CpeItem `xml:"cpe-item"`
+}
+
+// CpeItem has CPE information
+type CpeItem struct {
+	Name      string    `xml:"name,attr"`
+	Cpe23Item Cpe23Item `xml:"cpe23-item"`
+}
+
+// Cpe23Item has CPE 2.3 information
+type Cpe23Item struct {
+	Name string `xml:"name,attr"`
+}
+
+// Pair has fs and uri
+type Pair struct {
+	FS  string
+	URI string
+}
+
+func main() {
+	url := "http://static.nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.gz"
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Printf("HTTP error. errs: %s, url: %s", err, url)
+		return
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	b := bytes.NewBufferString(string(body))
+	reader, err := gzip.NewReader(b)
+	defer reader.Close()
+	if err != nil {
+		fmt.Printf("Failed to decompress NVD feedfile. url: %s, err: %s", url, err)
+		return
+	}
+	bytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		fmt.Printf("Failed to Read NVD feedfile. url: %s, err: %s", url, err)
+		return
+	}
+	cpeList := CpeList{}
+	if err = xml.Unmarshal(bytes, &cpeList); err != nil {
+		fmt.Printf("Failed to unmarshal. url: %s, err: %s", url, err)
+		return
+	}
+
+	uriList := []string{}
+	fsList := []string{}
+	for _, cpeItem := range cpeList.CpeItems {
+		uriList = append(uriList, cpeItem.Name)
+		fsList = append(fsList, cpeItem.Cpe23Item.Name)
+	}
+
+	pair := []Pair{}
+	shuffle(uriList)
+	shuffle(fsList)
+	for i := 0; i < 1000; i++ {
+		pair = append(pair, Pair{
+			FS:  fsList[i],
+			URI: uriList[i],
+		})
+	}
+
+	fmt.Println("---URI template---")
+	t := template.Must(template.ParseFiles("dictionary_test.tmpl"))
+	file, _ := os.Create(`./dictionary_test.go`)
+	defer file.Close()
+	t.Execute(file, map[string]interface{}{
+		"URI":  uriList,
+		"FS":   fsList,
+		"Pair": pair,
+	})
+}
+
+func shuffle(data []string) {
+	n := len(data)
+	for i := n - 1; i >= 0; i-- {
+		j := rand.Intn(i + 1)
+		data[i], data[j] = data[j], data[i]
+	}
+}
